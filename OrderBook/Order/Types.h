1f6ad79b3146bf60602d3218f76e7d367b3ca7e3
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2025-10-20 12:07:22 +0530

    [OME-2] feat: add Order Types and Enums
    Signed-off-by: vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>

---
+++ OrderBook/Order/Types.h
@@ -0,0 +1,42 @@
+//
+// Created by Vaasu Bisht on 20/10/25.
+//
+
+#pragma once
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <string>
+#include <memory>
+#include <cstdint>
+#include <utility>
+
+using Price = int64_t;
+using Quantity = uint64_t;
+using OrderId = uint64_t;
+using Count = uint64_t;
+using Symbol = std::string;
+using Timestamp = std::chrono::system_clock::time_point;
+
+enum Side
+{
+    BUY,
+    SELL
+};
+
+enum Type : char{
+    MARKET,
+    LIMIT,
+    STOP,
+    STOP_LIMIT
+};
+
+enum Status
+{
+    PENDING,
+    CANCELLED,
+    FULFILLED,
+    PARTIALLY_FULFILLED
+};
+
+#endif //TYPES_H
