6ddfb7498062e12aa8e576fb6ef5431eb20b5497
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2025-10-25 16:25:04 +0530

    [OME-7] refactor(Application): removed empty spaces
    Signed-off-by: vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>

--- Scheduler/OrderBookScheduler.h
+++ Scheduler/OrderBookScheduler.h
@@ -14,6 +14,7 @@
  * @class OrderBookScheduler
  * @extends Scheduler
  */
+
 class OrderBookScheduler final : public Scheduler {
 public:
  using SymbolToWorkerMap = std::unordered_map<Symbol, std::string>;
@@ -55,10 +56,6 @@ public:
  }
 
  void processOrder(OrderPtr order);
-
-
-
-
 };
 
 
