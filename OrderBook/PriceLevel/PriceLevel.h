1302a7a371c3ce21dab9ca9cacb48d2dd7ae3da3
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2025-11-11 13:21:26 +0000

    [OME-TIF]: refactor(PriceLevel): update matchOrders method to use reference for requested quantity and improve comments

--- OrderBook/PriceLevel/PriceLevel.h
+++ OrderBook/PriceLevel/PriceLevel.h
@@ -12,11 +12,12 @@
 
 /**
  * @brief Represents a single price point.
- *
+ * Each PriceLevel object maintains a list of orders at that price. 
+ * 
  * @details
- * Contains all the unmatched and active orders submitted at that price.
- * Orders are stored in FIFO by entry time.
- * Think of an order book like a building with floors, where each floor represents a different price.
+ *  - Orders are stored in FIFO by entry time.
+ *  - Think of an order book like a building with floors, where each floor represents a different price.
+ *  - Handles the logic matching, when price level is verified by OrderTracker
  */
 class PriceLevel{
 public:
@@ -25,8 +26,8 @@ public:
 private:
     Price mPrice; /// > Price to which this PriceLevel object corresponds.
     OrderList mOrders; /// > List of resting order at this price level.
-    Quantity mTotalQuantity; /// > Total quantity of all orders at this price.
-    Count mOrderCount; /// > Total number of orders at this price.
+    Quantity mTotalQuantity; /// > Total quantity of all units at this price. (liquidity at this price range)
+    Count mOrderCount; /// > Total number of pending orders at this price.
 public:
     /* Constructor */
     explicit PriceLevel(Price price);
@@ -74,11 +75,11 @@ public:
      *
      * @remarks
      * Justification of using raw pointer
-     * Returns a raw pointer here because the OrderBook (this class) retains exclusive
-     * ownership of all Order objects via std::unique_ptr<Orderz> stored in mOrders.
+     * Returns a raw pointer here because the PriceLevel (this class) retains exclusive
+     * ownership of all Order objects via std::unique_ptr<Order> stored in mOrders.
      * Returning a raw pointer provides safe, non-owning access to the first order
      * (FIFO) without transferring or sharing ownership. The caller can inspect or
-     * modify the Order through this pointer as long as the OrderBook remains alive
+     * modify the Order through this pointer as long as the PriceLevel remains alive
      * and does not remove the Order from mOrders. This design avoids unnecessary
      * shared_ptr reference counting overhead while still allowing safe access to
      * the managed object.
@@ -86,21 +87,44 @@ public:
      */
     [[nodiscard]] OrderRawPtr frontOrder() const;
 
-    struct MatchedTrade { OrderId restingOrderId{}; Quantity qty{}; Price price{};};
-    struct MatchResult { Quantity remaining; std::vector<MatchedTrade> trades; };
     /**
-     * @brief Match order at this price level up a specified quantity.
-     *
-     * Used when an order from opposite side (inBoundOrder) at same price level comes with
-     * open quantity `maxQty`.
-     * Follows FIFO rule, earlier order gets filled first.
-     *
-     * @details
-     * Iterate thought all the order in this price level until maxQty becomes zero.
-     *
-     * @return Amount of shares of inBoundOrder left and list of all trades
+     * @brief
+     * Details of a single trade execution i.e. which resting order was hit,
+     * the quantity traded, and the execution price.
+     * @todo Add timestamp
      */
-    MatchResult matchOrders(Quantity maxQty);
+    struct MatchedTrade { OrderId restingOrderId{}; Quantity qty{}; Price price{};};
+
+    /**
+     * Aggregate of all trades generated during the matching process for one incoming order.
+     */
+    struct MatchResult { std::vector<MatchedTrade> trades; };
+
+    /**
+     * @brief Attempts to match up to `qty` units of an incoming (opposite-side) order
+     *        against the resting orders in this price level.
+     * 
+     * This function is invoked when an inbound order is eligible to trade with the
+     * resting orders at the current price level.
+     * 
+     * @details
+     * Iterates over all resting orders at this price level, consuming as much of the
+     * requested quantity (`qty`) as possible until either:
+     * - the inbound order is fully filled, or
+     * - all resting orders at this level are exhausted.
+     * 
+     * It then generates a list of all individual trade executions that occurred
+     * during matching
+     * 
+     * @param[in,out] reqQty Quantity of the inbound order to match. Updated to remaining quantity.
+     * 
+     * @attention
+     * The parameter `reqQty` is passed by reference and will be decremented in place
+     * to reflect the remaining unfilled quantity of the inbound order after matching.
+     * 
+     * @return List of trades executed at this price level
+     */
+    MatchResult matchOrders(Quantity& reqQty);
 };
 
 
